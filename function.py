bfdb182537dfbd5db40cfbb9f6af041439c6a08d
Omar Belhoul <166892687+OmarBelhoul@users.noreply.github.com>
2025-09-26 23:40:31 +0100

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+def double_number(x):
+    return x*2
+
+print(double_number(5))
+
+def user_number(number):
+    return number*2
+
+number = int(input("type a number "))
+print(user_number(number))
